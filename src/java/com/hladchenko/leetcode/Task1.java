b551f93590b696d6df577a13fdaf04c3b69986a9
Ruslan Hladchenko <rugladchenko@gmail.com>
2021-12-06 20:22:11 +0200

    1. Two Sum

---
+++ src/java/com/hladchenko/leetcode/Task1.java
@@ -0,0 +1,34 @@
+/*
+    Given an array of integers nums and an integer target,
+     return indices of the two numbers such that they add up to target.
+    https://leetcode.com/problems/two-sum/
+ */
+package com.hladchenko.leetcode;
+
+import java.util.Arrays;
+
+public class Task1 {
+    public static void main(String[] args) {
+        int[] nums = {2,7,11,15};
+        int target = 9;
+        int[] nums2 = {3,2,4};
+        int target2 = 6;
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+
+        System.out.println(Arrays.toString(twoSum(nums, target)));
+        System.out.println(Arrays.toString(twoSum(nums2, target2)));
+        System.out.println(Arrays.toString(twoSum(nums3, target3)));
+    }
+
+    public static int[] twoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < nums.length; j++) {
+                if ((i != j) && (nums[i] + nums[j] == target)) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return null;
+    }
+}
