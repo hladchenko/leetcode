b5177f8c13c04ae3f4f9916ef102e8ea0bd4da35
Ruslan Hladchenko <rugladchenko@gmail.com>
2021-12-16 06:45:04 +0200

    1512. Number of Good Pairs (Refactor)

--- src/main/java/com/hladchenko/leetcode/easy/Task1512.java
+++ src/main/java/com/hladchenko/leetcode/easy/Task1512.java
@@ -9,8 +9,8 @@ public class Task1512 {
     public int numIdenticalPairs(int[] nums) {
         int goodPairsCount = 0;
         for (int i = 0; i < nums.length; i++) {
-            for (int j = 1; j < nums.length; j++) {
-                if (nums[i] == nums[j] && i < j) goodPairsCount++;
+            for (int j = 0; j < i; j++) {
+                if (nums[i] == nums[j]) goodPairsCount++;
             }
         }
         return goodPairsCount;
