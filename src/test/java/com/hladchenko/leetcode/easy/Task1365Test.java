91ab631ffd78fac79ee49809cfe3ca774f9963b2
Ruslan Hladchenko <rugladchenko@gmail.com>
2021-12-20 04:29:36 +0200

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ src/test/java/com/hladchenko/leetcode/easy/Task1365Test.java
@@ -0,0 +1,26 @@
+package com.hladchenko.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Task1365Test {
+
+    @Test
+    public void testSmallerNumberThanCurrent() {
+        //given
+        Task1365 task1365 = new Task1365();
+        int[] nums = {8, 1, 2, 2, 3};
+        int[] nums2 = {6, 5, 4, 8};
+        int[] nums3 = {7, 7, 7, 7};
+
+        //when
+        int[] actual = task1365.smallerNumbersThanCurrent(nums);
+        int[] actual2 = task1365.smallerNumbersThanCurrent(nums2);
+        int[] actual3 = task1365.smallerNumbersThanCurrent(nums3);
+
+        //then
+        Assert.assertArrayEquals(new int[]{4, 0, 1, 1, 3}, actual);
+        Assert.assertArrayEquals(new int[]{2, 1, 0, 3}, actual2);
+        Assert.assertArrayEquals(new int[]{0, 0, 0, 0}, actual3);
+    }
+}
