803bc91a7ba798d0b1e08f2dad6a50f8c48d0128
Ruslan Hladchenko <rugladchenko@gmail.com>
2021-12-30 11:54:31 +0200

    1379. Find a Corresponding Node of a Binary Tree in a Clone of That Tree

---
+++ src/main/java/com/hladchenko/leetcode/medium/Task1379.java
@@ -0,0 +1,34 @@
+/*
+    Given two binary trees original and cloned and given a reference
+     to a node target in the original tree.
+
+    The cloned tree is a copy of the original tree.
+
+    Return a reference to the same node in the cloned tree.
+
+    Note that you are not allowed to change any of the two trees or the target
+    node and the answer must be a reference to a node in the cloned tree.
+    https://leetcode.com/problems/find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/
+ */
+package com.hladchenko.leetcode.medium;
+
+public class Task1379 {
+
+    public final TreeNode getTargetCopy(final TreeNode original, final TreeNode cloned, final TreeNode target) {
+        if (target.val == cloned.val) {
+            return cloned;
+        }
+        TreeNode found;
+        if (cloned.left != null) {
+            found = getTargetCopy(original, cloned.left, target);
+            if (found != null) {
+                return found;
+            }
+        }
+        if (cloned.right != null) {
+            found = getTargetCopy(original, cloned.right, target);
+            return found;
+        }
+        return null;
+    }
+}
---
+++ src/test/java/com/hladchenko/leetcode/medium/Task1379Test.java
@@ -0,0 +1,41 @@
+package com.hladchenko.leetcode.medium;
+
+import com.sun.source.tree.Tree;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Task1379Test {
+
+    @Test
+    public void findTargetNodeInClonedTree() {
+        // Arrange
+        Task1379 task1379 = new Task1379();
+        TreeNode original = new TreeNode(7,
+                new TreeNode(4),
+                new TreeNode(3, new TreeNode(6), new TreeNode(19)));
+        TreeNode original2 = new TreeNode(7);
+        TreeNode original3 = new TreeNode(8, null,
+                new TreeNode(6, null,
+                        new TreeNode(5, null, new TreeNode(4, null,
+                                new TreeNode(3, null,
+                                        new TreeNode(2, null,
+                                                new TreeNode(1)))))));
+
+        TreeNode target = new TreeNode(3);
+        TreeNode target2 = new TreeNode(7);
+        TreeNode target3 = new TreeNode(4);
+
+        // Act
+        TreeNode actual = task1379.getTargetCopy(original, original, target);
+        TreeNode actual2 = task1379.getTargetCopy(original2, original2, target2);
+        TreeNode actual3 = task1379.getTargetCopy(original3, original3, target3);
+
+        // Assert
+        assert actual != null;
+        Assert.assertEquals(3, actual.val);
+        assert actual2 != null;
+        Assert.assertEquals(7, actual2.val);
+        assert actual3 != null;
+        Assert.assertEquals(4, actual3.val);
+    }
+}
