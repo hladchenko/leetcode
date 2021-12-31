cd473d443b8755f7e47a32de53203b5c6d686124
Ruslan Hladchenko <rugladchenko@gmail.com>
2021-12-31 05:33:42 +0200

    807. Max Increase to Keep City Skyline

---
+++ src/test/java/com/hladchenko/leetcode/medium/Task807Test.java
@@ -0,0 +1,23 @@
+package com.hladchenko.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Task807Test {
+
+    @Test
+    public void maxTotalSumHeightCanBeIncreased() {
+        // Given
+        Task807 task807 = new Task807();
+        int[][] grid = {{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
+        int[][] grid2 = {{0, 0, 0}, {0, 0, 0}, {0, 0, 0}};
+
+        // When
+        int actual = task807.maxIncreaseKeepingSkyline(grid);
+        int actual2 = task807.maxIncreaseKeepingSkyline(grid2);
+
+        // Then
+        Assert.assertEquals(35, actual);
+        Assert.assertEquals(0, actual2);
+    }
+}
