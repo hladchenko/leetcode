5720c9a6e1c4e12219e78a9860d9df48ef98799f
Ruslan Hladchenko <rugladchenko@gmail.com>
2022-01-04 05:26:52 +0200

    938. Range Sum of BST (refactor)

--- src/main/java/com/hladchenko/leetcode/easy/Task938.java
+++ src/main/java/com/hladchenko/leetcode/easy/Task938.java
@@ -11,15 +11,14 @@ public class Task938 {
 
     public int rangeSumBST(TreeNode root, int low, int high) {
 
+        if (root == null) {
+            return 0;
+        }
+
         int sum = 0;
 
-        if (root.left != null) {
-            sum += rangeSumBST(root.left, low, high);
-        }
-
-        if (root.right != null) {
-            sum += rangeSumBST(root.right, low, high);
-        }
+       sum += rangeSumBST(root.left, low, high);
+       sum += rangeSumBST(root.right, low, high);
 
         if (root.val >= low && root.val <= high) {
             sum += root.val;
