626e1ab8c9bc2f5d642e63751db1e6c29efcd116
Ruslan Hladchenko <rugladchenko@gmail.com>
2022-01-07 04:29:57 +0200

    1486. XOR Operation in an Array (refactor)

--- src/main/java/com/hladchenko/leetcode/easy/Task1486.java
+++ src/main/java/com/hladchenko/leetcode/easy/Task1486.java
@@ -13,7 +13,7 @@ public class Task1486 {
     public int xorOperation(int n, int start) {
         int xor = start;
         for (int i = 1; i < n; i++) {
-            xor = xor ^ (start + 2 * i);
+            xor ^= start + 2 * i;
         }
         return xor;
     }
