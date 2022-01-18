d10616091f394bea007e3b068f0e7269671e72a4
Ruslan Hladchenko <rugladchenko@gmail.com>
2022-01-18 23:06:29 +0200

    1588. Sum of All Odd Length Subarrays (TODO)

---
+++ src/main/java/com/hladchenko/leetcode/easy/Task1588.java
@@ -0,0 +1,17 @@
+/*
+    Given an array of positive integers arr, calculate the sum of all possible odd-length subarrays.
+
+    A subarray is a contiguous subsequence of the array.
+
+    Return the sum of all odd-length subarrays of arr.
+    https://leetcode.com/problems/sum-of-all-odd-length-subarrays/
+ */
+package com.hladchenko.leetcode.easy;
+
+public class Task1588 {
+
+    public int sumOddLengthSubarrays(int[] arr) {
+
+        return 0;
+    }
+}
---
+++ src/test/java/com/hladchenko/leetcode/easy/Task1588Test.java
@@ -0,0 +1,23 @@
+package com.hladchenko.leetcode.easy;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class Task1588Test {
+
+    @Test
+    public void sumOddLengthSubarrays() {
+        Task1588 task1588 = new Task1588();
+        int[] arr = {1, 4, 2, 5, 3};
+        int[] arr2 = {1, 2};
+        int[] arr3 = {10, 11, 12};
+
+        int result = task1588.sumOddLengthSubarrays(arr);
+        int result2 = task1588.sumOddLengthSubarrays(arr2);
+        int result3 = task1588.sumOddLengthSubarrays(arr3);
+
+        Assert.assertEquals(58, result);
+        Assert.assertEquals(3, result);
+        Assert.assertEquals(66, result);
+    }
+}
