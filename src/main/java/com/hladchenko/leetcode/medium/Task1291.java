2c15a0885d8030d4f54b9bad9e79617acbdb6356
Ruslan Hladchenko <rugladchenko@gmail.com>
2022-01-24 23:13:23 +0200

    1291. Sequential Digits (TODO)

---
+++ src/main/java/com/hladchenko/leetcode/medium/Task1291.java
@@ -0,0 +1,10 @@
+package com.hladchenko.leetcode.medium;
+
+import java.util.List;
+
+public class Task1291 {
+
+    public List<Integer> sequentialDigits(int low, int high) {
+        return null;
+    }
+}
---
+++ src/test/java/com/hladchenko/leetcode/medium/Task1291Test.java
@@ -0,0 +1,26 @@
+package com.hladchenko.leetcode.medium;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.List;
+
+public class Task1291Test {
+
+    @Test
+    public void sequentialDigits() {
+        Task1291 task1291 = new Task1291();
+
+        List<Integer> result = task1291.sequentialDigits(100, 300);
+
+        Assert.assertEquals(result, List.of(123, 234));
+    }
+
+    public void sequentialDigitsSecond() {
+        Task1291 task1291 = new Task1291();
+
+        List<Integer> result = task1291.sequentialDigits(1000, 13000);
+
+        Assert.assertEquals(result, List.of(1234, 2345, 3456, 4567, 5678, 6789, 12345));
+    }
+}
