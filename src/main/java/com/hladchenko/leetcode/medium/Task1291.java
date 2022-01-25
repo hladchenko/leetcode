2fe663d858c6c5792cf290a0464f358af598fda8
Ruslan Hladchenko <rugladchenko@gmail.com>
2022-01-25 08:46:01 +0200

    1291. Add Sequential Digits first try (TODO)

--- src/main/java/com/hladchenko/leetcode/medium/Task1291.java
+++ src/main/java/com/hladchenko/leetcode/medium/Task1291.java
@@ -1,10 +1,32 @@
 package com.hladchenko.leetcode.medium;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Task1291 {
 
     public List<Integer> sequentialDigits(int low, int high) {
-        return null;
+        List<Integer> list = new ArrayList<>();
+        for (int i = low; i < high; i++) {
+            char[] arr = String.valueOf(i).toCharArray();
+            boolean isSequential = true;
+            int previous = Integer.valueOf(arr[0]);
+            for (int j = 1; j < arr.length; j++) {
+                if (previous + 1 != Integer.valueOf(arr[j])) {
+                    isSequential = false;
+                }
+                previous = Integer.valueOf(arr[j]);
+            }
+            if (isSequential) {
+                list.add(i);
+            }
+        }
+        return list;
+    }
+
+    public List<Integer> sequentialDigitsSecondApproach(int low, int high) {
+        List<Integer> list = new ArrayList<>();
+        // TODO
+        return list;
     }
 }
--- src/test/java/com/hladchenko/leetcode/medium/Task1291Test.java
+++ src/test/java/com/hladchenko/leetcode/medium/Task1291Test.java
@@ -16,10 +16,29 @@ public class Task1291Test {
         Assert.assertEquals(result, List.of(123, 234));
     }
 
+    @Test
     public void sequentialDigitsSecond() {
         Task1291 task1291 = new Task1291();
 
         List<Integer> result = task1291.sequentialDigits(1000, 13000);
+
+        Assert.assertEquals(result, List.of(1234, 2345, 3456, 4567, 5678, 6789, 12345));
+    }
+
+    @Test
+    public void sequentialDigitsSecondApproach() {
+        Task1291 task1291 = new Task1291();
+
+        List<Integer> result = task1291.sequentialDigitsSecondApproach(100, 300);
+
+        Assert.assertEquals(result, List.of(123, 234));
+    }
+
+    @Test
+    public void sequentialDigitsSecondSecondApproach() {
+        Task1291 task1291 = new Task1291();
+
+        List<Integer> result = task1291.sequentialDigitsSecondApproach(1000, 13000);
 
         Assert.assertEquals(result, List.of(1234, 2345, 3456, 4567, 5678, 6789, 12345));
     }
